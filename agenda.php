c1230fc0f404947491885f69c79816d05b9af65a
Neemoasb <51499704+Neemoasb@users.noreply.github.com>
2020-05-08 14:20:56 -0300

    Agenda

---
+++ agenda.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Show da Física</title>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" type="image/x-png" href="img/ausronauta.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" href="css/index.css">
+		
+		<link rel="stylesheet" href="css/atomo.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+	</head>
+
+	<body>
+		<?php include 'menu.php'; ?>
+
+
+
+		<div class="agenda fundo">
+			<input type="radio" name="nav-agenda" id="n1" checked>
+			<input type="radio" name="nav-agenda" id="n2">
+			<input type="radio" name="nav-agenda" id="n3">
+
+			<p class="title-slide">Agenda</p>
+
+			<div class="slide">
+				<div class="item-list">
+					<div class="description">
+						<p class="info-title">Itapetininga</p>
+						<p class="info-sub">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque omnis quibusdam repellat voluptas temporibus </p>
+					</div>
+
+					<div class="banners">
+						<div class="date">
+							<img src="img/banner/cartaz 25.09.19.jpeg" alt="">
+
+							<div class="saiba-mais">
+								<p>25 set</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia 19.10.png" alt="">
+
+							<div class="saiba-mais">
+								<p>19 out</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia feira de ciencias outubro.png" alt="">
+
+							<div class="saiba-mais">
+								<p>24 out</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="item-list">
+					<div class="description">
+						<p class="info-title">Itú</p>
+						<p class="info-sub">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque omnis quibusdam repellat voluptas temporibus</p>
+					</div>
+
+					<div class="banners">
+						<div class="date">
+							<img src="img/banner/cartaz 25.09.19.jpeg" alt="">
+
+							<div class="saiba-mais">
+								<p>18 fev</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia 19.10.png" alt="">
+
+							<div class="saiba-mais">
+								<p>20 mar</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia feira de ciencias outubro.png" alt="">
+
+							<div class="saiba-mais">
+								<p>05 abr</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="item-list">
+					<div class="description">
+						<p class="info-title">Itapeva</p>
+						<p class="info-sub">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque omnis quibusdam repellat voluptas temporibus</p>
+					</div>
+
+					<div class="banners">
+						<div class="date">
+							<img src="img/banner/cartaz 25.09.19.jpeg" alt="">
+
+							<div class="saiba-mais">
+								<p>02 jun</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia 19.10.png" alt="">
+
+							<div class="saiba-mais">
+								<p>13 ago</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+
+						<div class="date">
+							<img src="img/banner/show dia feira de ciencias outubro.png" alt="">
+
+							<div class="saiba-mais">
+								<p>11 nov</p>
+								<a href="">Escola: Nao sei oq</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="navigation">
+				<label class="lbl-slide s1" for="n1"><div class="circle"></div></label>
+				<label class="lbl-slide s2" for="n2"><div class="circle"></div></label>
+				<label class="lbl-slide s3" for="n3"><div class="circle"></div></label>
+			</div>
+		</div>
+
+		
+
+        <?php include 'footer.php'; ?>
+		
+	</body>
+</html>
--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
     <div class="links-menu">
         <a class="nav-link" href="./">Início</a>
         <a class="nav-link" href="./sobre.php">Sobre</a>
-        <a class="nav-link" href="./">Agenda</a>
+        <a class="nav-link" href="./agenda.php">Agenda</a>
         <a class="nav-link" href="./galeria.php">Galeria</a>
         <a class="nav-link" href="./contato.php">Contato</a>
         <a class="nav-link" href="./apoio.php">Patrocine</a>
